26a5d99b6845687524a58a5e4c2b9a81d42a6a42
anakettl <anakettl@gmail.com>
2021-10-08 14:22:42 -0300

    feat(table): adicionar metodo de tabela frequencia

--- lib/calc.rb
+++ lib/calc.rb
@@ -2,4 +2,12 @@ class Calc
   def self.media(x,y)
     (x+y)/2
   end
+
+  def self.moda(arr)
+    arr.tally
+  end
+
+  def self.tabela_frequencia(arr)
+    arr.group_by(&:itself).map { |k,v| "#{k} => #{v.count}" }
+  end
 end
--- lib/text.rb
+++ lib/text.rb
@@ -1,11 +1,56 @@
 class Text
   def self.media
-    puts 'Digite 2 valores'
-    x = gets
-    y = gets
+    puts 'Digite os valores para calcular a media'
+    puts 'Digite x para concluir a digitação'
+
+    arr = []
+    loop do 
+      input = gets.chomp
+      break if input.eql? "x"
+
+      arr << input.to_i
+    end
     
     media = Calc.media(x.to_i,y.to_i)
 
     puts "A media dos valores é: #{media}"
+  end
+
+  def self.moda
+    puts 'Digite os valores para calcular a moda'
+    puts 'Digite x para concluir a digitação'
+
+    arr = []
+    loop do 
+      input = gets.chomp
+      break if input.eql? "x"
+
+      arr << input.to_i
+    end
+    
+    moda = Calc.moda(arr)
+
+    puts "A moda dos valores é: #{moda}"
+  end
+
+  def self.tabela_frequencia
+    puts 'Digite os valores para criar a tabela frequencia'
+    puts 'Digite x para concluir a digitação'
+
+    arr = []
+    loop do 
+      input = gets.chomp
+      break if input.eql? "x"
+
+      arr << input.to_i
+    end
+    
+    tabela_frequencia = Calc.tabela_frequencia(arr)
+
+    puts "A tabela frequencia é: "
+    puts "chave => frequencia"
+    tabela_frequencia.each do |linha|
+      puts linha
+    end
   end
 end
